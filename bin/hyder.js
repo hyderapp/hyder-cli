6d96cc00ae442a0a1f39dd1eed4f780a6257ed85
bencode <bencode@163.com>
2019-12-18 12:05:11 +0800

    save for refactor

--- bin/hyder.js
+++ bin/hyder.js
@@ -7,5 +7,5 @@ const main = require('../lib/index');
 process.title = 'hyder/cli';
 
 const argv = minimist(process.argv.slice(2));
-const { _: [name = 'help', sub = 'index'], ...opts } = argv;
-main(name, sub, opts);
+const { _: [name = 'help', action = 'index', param], ...opts } = argv;
+main(name, action, param, opts);
