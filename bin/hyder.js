890321cf65a40e8a2605311769111ff568849b7a
bencode <bencode@163.com>
2019-12-18 16:11:11 +0800

    finish commandline interface

--- bin/hyder.js
+++ bin/hyder.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-const minimist = require('minimist');
 const main = require('../lib/index');
-
 
 process.title = 'hyder/cli';
 
-const argv = minimist(process.argv.slice(2));
-const { _: [name = 'help', action = 'index', param], ...opts } = argv;
-main(name, action, param, opts);
+main(process.argv.slice(2));
